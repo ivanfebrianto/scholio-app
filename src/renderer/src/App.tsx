c9942c83e256683c9c4d8041b6cdaca34424cbef
ivanfebrianto <specopskit@gmail.com>
2025-04-13 22:36:56 +0700

    add styling setup

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,4 +1,8 @@
 function App() {
-  return <div></div>
+  return (
+    <div className="flex h-full items-center justify-center">
+      <span className="text-4xl text-blue-500">Hello Electrons</span>
+    </div>
+  )
 }
 export default App
